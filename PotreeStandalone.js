f3321d1254b3ccddf5e87159ce5affee667c5ed9
Kishan <kishan.dedakia@3drobotics.com>
2017-11-10 13:13:20 -0800

    Bugfix: Remove scriptPath check

--- PotreeStandalone.js
+++ PotreeStandalone.js
@@ -45865,15 +45865,17 @@ module.exports = Version;
 },{}],10:[function(require,module,exports){
 const LRU = require('./LRU');
 
-let scriptPath = null;
-if (document.currentScript.src) {
-	scriptPath = new URL(document.currentScript.src + '/..').href;
-	if (scriptPath.slice(-1) === '/') {
-		scriptPath = scriptPath.slice(0, -1);
-	}
-} else {
-	console.error('Potree was unable to find its script path using document.currentScript. Is Potree included with a script tag? Does your browser support this function?');
-}
+let scriptPath = '';
+
+// if (document.currentScript.src) {
+// 	scriptPath = new URL(document.currentScript.src + '/..').href;
+// 	if (scriptPath.slice(-1) === '/') {
+// 		scriptPath = scriptPath.slice(0, -1);
+// 	}
+// } else {
+// 	console.error('Potree was unable to find its script path using document.currentScript. Is Potree included with a script tag? Does your browser support this function?');
+// }
+// scriptPath = '';
 
 let LRUInstance = null;
 
@@ -45884,7 +45886,7 @@ module.exports = {
 		suffix: 'RC'
 	},
 	framenumber: 0,
-	pointBudget: 1 * 1000 * 1000,
+	pointBudget: 1 * 1000 * 5000,
 	scriptPath: scriptPath,
 	resourcePath: scriptPath + '/resources',
 	getLRU: function () {
--- src/context.js
+++ src/context.js
@@ -1,14 +1,16 @@
 const LRU = require('./LRU');
 
-let scriptPath = null;
-if (document.currentScript.src) {
-	scriptPath = new URL(document.currentScript.src + '/..').href;
-	if (scriptPath.slice(-1) === '/') {
-		scriptPath = scriptPath.slice(0, -1);
-	}
-} else {
-	console.error('Potree was unable to find its script path using document.currentScript. Is Potree included with a script tag? Does your browser support this function?');
-}
+let scriptPath = '';
+
+// if (document.currentScript.src) {
+// 	scriptPath = new URL(document.currentScript.src + '/..').href;
+// 	if (scriptPath.slice(-1) === '/') {
+// 		scriptPath = scriptPath.slice(0, -1);
+// 	}
+// } else {
+// 	console.error('Potree was unable to find its script path using document.currentScript. Is Potree included with a script tag? Does your browser support this function?');
+// }
+// scriptPath = '';
 
 let LRUInstance = null;
 
